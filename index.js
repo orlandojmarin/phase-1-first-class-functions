c1a9dd25ba8cff70feeba134d67f9aaaf21edabd
Orlando Marin <orlandojermainemarin@gmail.com>
2021-02-25 20:38:54 -0800

    commit tree 11ebb56a9306ccd6a698843d383f0bb6308ebc21

---
+++ index.js
@@ -0,0 +1,13 @@
+function receivesAFunction(callback) {
+    return callback();
+}
+
+function returnsANamedFunction() {
+    return receivesAFunction;
+}
+
+function returnsAnAnonymousFunction() {
+    return function () {
+        console.log("Hello World!");
+    }
+}
